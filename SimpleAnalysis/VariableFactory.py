d5515ba3260d0e5324cb7589358303745a3d0ad6
Karol Krizka <kkrizka@gmail.com>
2012-02-20 20:56:12 -0800

    Made variable names more human readable.

--- SimpleAnalysis/VariableFactory.py
+++ SimpleAnalysis/VariableFactory.py
@@ -21,10 +21,23 @@ def setEventID(eventID):
 
 
 def get(variable,*args):
+    # Build a key for arguments...
+    args_key=''
+    for arg in args:
+        if type(arg)==float or type(arg)==int:
+            args_key+=(str(arg)+',')
+        elif type(arg)==str:
+            args_key+=('"'+str(arg)+'",')
+        else:
+            args_key+=(arg.__class__.__name__+',')
+    if args_key!='':
+        args_key=args_key[:-1]
+
+    # Create
     key=str(variable)+str(args)
     if(key not in _cache):
         _cache[key]=variable(*args)
-        _cache[key].name=_cache[key].name+str(args)
+        _cache[key].name=_cache[key].name+'('+args_key+')'
         _cache[key].particles=_particles
         _cache[key].eventID=_eventID
 
