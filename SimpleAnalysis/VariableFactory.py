8f76daa9f405c5e05d413af4aa15b12d4ff3b326
Karol Krizka <kkrizka@uchicago.edu>
2013-01-28 14:31:54 -0600

    VariableFactory.get() can now set attributes to variables via named arguments..

--- SimpleAnalysis/VariableFactory.py
+++ SimpleAnalysis/VariableFactory.py
@@ -18,13 +18,17 @@ def setEventFile(eventfile):
     for key in _cache:
         _cache[key].eventfile=eventfile
 
-def get(variable,*args):
-    # Create
-    key=str(variable)+str(args)
+def get(variable,*args,**kwargs):
+    # Determine the key
+    key=str(variable)+str(args)+str(kwargs)
     if key not in _cache:
         _cache[key]=variable(*args)
         _cache[key].event=_event
         _cache[key].eventfile=_eventfile
+        # Set the extra keyword args
+        for k in kwargs:
+            v=kwargs[k]
+            setattr(_cache[key],k,v)
 
     return _cache[key]
 
