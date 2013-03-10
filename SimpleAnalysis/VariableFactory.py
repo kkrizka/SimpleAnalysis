70d1540840b163621e5cf2d67e2c5d10251f18c5
Karol Krizka <kkrizka@uchicago.edu>
2013-03-10 17:05:18 -0500

    VariableFactory now supports hidden attributes in get that are not used to create the cache key.

--- SimpleAnalysis/VariableFactory.py
+++ SimpleAnalysis/VariableFactory.py
@@ -19,16 +19,26 @@ def setEventFile(eventfile):
         _cache[key].eventfile=eventfile
 
 def get(variable,*args,**kwargs):
+    # Remove hidden arguments from kwargs
+    hiddenargs={}
+    for key in kwargs:
+        if key[0]=='_': #hidden
+            hiddenargs[key[1:]]=kwargs[key]
+    for key in hiddenargs:
+        del kwargs['_'+key]
     # Determine the key
     key=str(variable)+str(args)+str(kwargs)
     if key not in _cache:
         _cache[key]=variable(*args)
         _cache[key].event=_event
         _cache[key].eventfile=_eventfile
-        # Set the extra keyword args
-        for k in kwargs:
-            v=kwargs[k]
-            setattr(_cache[key],k,v)
+    # Set the extra keyword args
+    for k in kwargs:
+        v=kwargs[k]
+        setattr(_cache[key],k,v)
+    for k in hiddenargs:
+        v=hiddenargs[k]
+        setattr(_cache[key],k,v)
 
     return _cache[key]
 
