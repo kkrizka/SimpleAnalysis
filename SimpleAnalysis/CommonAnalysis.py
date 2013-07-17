d0cd760cf6c8cf2b23129225772a1c522fb62164
Karol Krizka <kkrizka@uchicago.edu>
2013-07-17 12:43:05 -0500

    VariableFlagCut and VariableCut now support cutflow histograms.

--- SimpleAnalysis/CommonAnalysis.py
+++ SimpleAnalysis/CommonAnalysis.py
@@ -69,12 +69,12 @@ class VariableCut(Analysis.Cut):
     ##           (Default: False)
     def __init__(self,variable,minVal,invert=False):
         Analysis.Cut.__init__(self,invert)
-        self.thevariable=variable
+        self.variable=variable
         self.minVal=minVal
 
     ## Cut method
     def cut(self):
-        value=self.thevariable.value()
+        value=self.variable.value()
         if type(value)==tuple:
             value=value[0]
         if(value<self.minVal):
@@ -103,6 +103,8 @@ class VariableEqualCut(Analysis.Cut):
 
 ## A generic cut that uses any variable and rejects events that have the
 ## variable equal to zero.
+##
+## Binning for cutflow histograms is defined to be 0-2, with 2 bins.
 class VariableFlagCut(Analysis.Cut):
     ## Arguments:
     ## - variable: An Analysis.Variable object that cuts will be run on
@@ -111,11 +113,15 @@ class VariableFlagCut(Analysis.Cut):
     ##           (Default: False)
     def __init__(self,variable,invert=False):
         Analysis.Cut.__init__(self,invert)
-        self.thevariable=variable
+        self.variable=variable
+
+        self.variable.minval=0
+        self.variable.maxval=2
+        self.variable.nbins=2
 
     ## Cut method
     def cut(self):
-        value=self.thevariable.value()
+        value=self.variable.value()
         if value==0: return True
         else: return False
 
